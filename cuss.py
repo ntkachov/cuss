5952d4308eca8212307e2d8b837110fcffbb4a8a
nick <ntkachov@gmail.com>
2012-03-27 18:38:26 -0400

    Added most css parsing

--- cuss.py
+++ cuss.py
@@ -5,30 +5,103 @@ import os
 import re
 from HTMLParser import HTMLParser
 
-path = "./"
-if(len(sys.argv) > 1):
-	path = sys.argv[1]
+#CSS Parsing 
+def parseCSSFile(filename, feed):
+	#check for matched parens. (Unmatched parens will cause errors)
+	if(checkParens(feed) != 0):
+		print ("Unmatched brackets in css file: " + filename)
+		exit()	
+	
+	#get rid of @media 
+	feed = removeMedia(feed)	
+	newfeed = re.sub('//.*?\n|/\*.*?\*/', '', feed, 0, re.DOTALL)
+	newfeed = re.sub(r'\{.*?\}','', newfeed, 0,  re.DOTALL)
+	print newfeed
+	tags = newfeed.split()
+	tags = sortTags(tags)	
+	splitAttrs(tags);
 
-folderfiles = os.listdir(path);
+def checkParens(feed):
+	parenCount = 0
+	for c in feed:
+		if(c == "{"):
+			parenCount +=1
+		elif(c == "}"):
+			parenCount -=1
+	return parenCount
+		
+def removeMedia(feed):
+	newfeed = ""
+	insert = True
+	parencount = 0
+	betweenbrackets = False
+	for c in feed:
+		if(c == '['):
+			betweenbrackets = True;
+		elif(c == ']'):
+			betweenbrackets = False;
+		if (not (betweenbrackets and re.match('\s', c))) :	
+			
+				
+			if(c == "@"):
+				insert = False
+			elif(not insert and c == "{"):
+				insert = True
+				parencount += 1
+			elif(insert and parencount > 0):
+				if( c == "{" ):
+					parencount +=1
+				elif( c == "}" ):
+					parencount -=1
+				if(parencount != 0):
+					newfeed += c
+			else:
+				if(insert):
+					newfeed += c
+	return newfeed
 
-fileAttrs = ["href"];
-checkTags = ["style"];
+def sortTags(tags):
+	newtags = []
+	for tag in tags:
+		if "[" in tag:
+			tag = re.sub(r'\:.*', '', tag)
+			tag = re.sub(',','', tag)
+			newtags.append(tag)
+	newtags = sorted(set(newtags))
+	return newtags
+
+def splitAttrs(tags):
+	attrs = {}
+	for tag in tags:
+		print tag
+		if('[' in tag):
+		 	attr = re.findall('\[.*?\]', tag);
+			print attr
+
+#HTML parsing and checking.
+
+fileAttrs = ["href"]
+checkTags = ["style"]
 checkAttrs = ["id", "class"]
 
 class cussParser(HTMLParser):
 	def handle_starttag(self, tag, attrs):
 		print tag + " : " + str(attrs) 
 
-def parseCSSFile(feed):
-	newfeed = re.sub(r'\{.*?\}','', feed, 0,  re.DOTALL)
-	print newfeed
 
-for fname in folderfiles:
-	if(".css" in fname):
-		cf = open(os.path.join(path, fname), 'r')
-		parseCSSFile(cf.read());
+#Program initialization
+def main():
+	path = "./"
+	if(len(sys.argv) > 1):
+		path = sys.argv[1]
+
+	folderfiles = os.listdir(path)
+
+	for fname in folderfiles:
+		if(".css" in fname):
+			cf = open(os.path.join(path, fname), 'r')
+			parseCSSFile(fname, cf.read())
 
 
-#index_f = open(path, 'r')
-#parser = cussParser()
-#parser.feed(index_f.read())
+main()
+

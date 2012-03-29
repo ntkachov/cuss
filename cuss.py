2c1f155ac0d0225345ad15b64a750f0fb0b77a01
ntkachov <ntkachov@ntkachov-Dimension-8250.(none)>
2012-03-29 18:34:42 -0400

    Created propper trees to follow the path of child selectors and combination selectors. Also refactored to use a class structure. Will create a propper class structure after dinner

--- cuss.py
+++ cuss.py
@@ -5,29 +5,39 @@ import os
 import re
 from HTMLParser import HTMLParser
 
-#CSS Parsing 
-def parseCSSFile(filename, feed):
-	#check for matched parens. (Unmatched parens will cause errors)
-	if(checkParens(feed) != 0):
-		print ("Unmatched brackets in css file: " + filename)
-		exit()	
-	
-	#get rid of @media. Nested brackets make regex harder. 
-	feed = removeMedia(feed)	
+#Class to store everything propperly
+class CSSFile:
+	def __init__(self, filename, feed):
+		self.file_name = filename
+		self.child_tree = {}
+		self.selector_mark = {}	
 
-	#remove Any comments and anything between brackets.
-	newfeed = re.sub('//.*?\n|/\*.*?\*/', '', feed, 0, re.DOTALL)
-	newfeed = re.sub('\{.*?\}','&', newfeed, 0,  re.DOTALL)
-	#replace all whitespace with a plus symbol
-	newfeed = re.sub(r'\s+', '+', newfeed, 0, re.MULTILINE)
-	#remove any intances of the plus symbol interacting with the ampersand
-	newfeed = re.sub("\+&\+|\+&|&\+", "&", newfeed)
-	tags = newfeed.split("&")
-	tags = sortTags(tags)	
-	attributes = splitAttrs(tags)
-	print attributes
+		#check for matched parens. (Unmatched parens will cause errors)
+		if(checkParens(feed) != 0):
+			print ("Unmatched brackets in css file: " + filename)
+			exit()	
+		
+		#get rid of @media. Nested brackets make regex harder. 
+		feed = removeMedia(feed)	
+
+		#remove Any comments and anything between brackets.
+		newfeed = re.sub('//.*?\n|/\*.*?\*/', '', feed, 0, re.DOTALL)
+		newfeed = re.sub('\{.*?\}','&', newfeed, 0,  re.DOTALL)
+		#replace all whitespace with a plus symbol
+		newfeed = re.sub(r'\s+', '+', newfeed, 0, re.MULTILINE)
+		#remove any intances of the plus symbol interacting with the ampersand
+		newfeed = re.sub("\+&\+|\+&|&\+|,\+|\+\+\+", "&", newfeed)
+		#print newfeed
+		tags = newfeed.split("&")
+		#print tags
+		tags = sortTags(tags)	
+		#print tags
+		attributes = splitAttrs(tags)
+		createTagTree(attributes);
+		#print attributes
 
 def checkParens(feed):
+	#Count the difference between the number of opening brackets and closing brackets.
 	parenCount = 0
 	for c in feed:
 		if(c == "{"):
@@ -46,7 +56,8 @@ def removeMedia(feed):
 			betweenbrackets = True
 		elif(c == ']'):
 			betweenbrackets = False
-		if (not (betweenbrackets and re.match('\s', c))) :	
+
+		if (not (betweenbrackets and re.match('\s', c))) :	#remove extra whitespace.
 			if(c == "@"):
 				insert = False
 			elif(not insert and c == "{"):
@@ -67,25 +78,62 @@ def removeMedia(feed):
 def sortTags(tags):
 	newtags = []
 	for tag in tags:
+		tag = re.sub('\*', '', tag);
+		tag = re.sub('(?<=\w)\#(?=\w)', "&#", tag) #Will replace any "h1.class" or "h1#id" with an &. 
+		tag = re.sub('(?<=\w)\.(?=\w)', "&.", tag) #Will replace any "h1.class" or "h1#id" with an &. 
 		if "[" in tag:
-			tag = re.sub(r'\:.*', '', tag)
-			tag = re.sub(',','', tag)
-			newtags.append(tag)
+			tag = re.sub(r'\:.*|', '', tag)
+		newtags.append(tag)
 	newtags = sorted(set(newtags))
 	return newtags
 
 def splitAttrs(tags):
 	attrs = {}
 	for tag in tags:
+		origintag = tag
 		if('[' in tag):
 		 	attr = re.findall('\[.*?\]', tag)
 			origintag = tag[0: int(tag.find("["))]
 			if(origintag[-1] == "+"): origintag = origintag[0:-1]
 			if origintag not in attrs:
 				attrs[origintag] = []
-			attrs[origintag].append(attr)
+			attrs[origintag] = attr
+		else:
+			attrs[origintag] = ''
 	return attrs
 
+def createTagTree(attrs):
+	def treeType(split, s_tree, tag, atts):
+			children = t.split(split)
+			node = s_tree
+			for c in children:
+				if(c in node):
+					node = node[c]
+				else:
+					node[c] = {}
+					node = node[c]
+			for a in atts:
+				node[a] = False;
+			#s_tree =  dict(s_tree.items() + node.items())
+			return s_tree 
+		 
+	childTree = {};
+	selectorTree = {}
+	selectorMark = {}
+	for t in attrs: 
+		selectorMark[t] = False
+		if( "+" in t ): #handle child case
+			childTree = treeType("+", childTree, t, attrs[t])
+		if( "&" in t): #handle multi case
+			selectorTree = treeType("&", selectorTree, t, attrs[t])	
+	print attrs
+	
+	print childTree
+	print selectorTree
+	print selectorMark
+			
+				
+					
 
 #HTML parsing and checking.
 
@@ -109,7 +157,7 @@ def main():
 	for fname in folderfiles:
 		if(".css" in fname):
 			cf = open(os.path.join(path, fname), 'r')
-			parseCSSFile(fname, cf.read())
+			CSSFile(fname, cf.read())
 
 
 main()
